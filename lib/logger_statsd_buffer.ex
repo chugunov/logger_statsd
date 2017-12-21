31c4f3c98b2b157d6584a5e0d019530a5d40a105
Andy <flashgame73@gmail.com>
2017-12-21 19:00:23 +0400

    Edit statsd key name

--- lib/logger_statsd_buffer.ex
+++ lib/logger_statsd_buffer.ex
@@ -6,26 +6,17 @@ defmodule LoggerStatsd.Buffer do
 
   @spec write(list(term)) :: :ok
   def write(counter_list) do
-    hostname = get_hostname()
     if length(counter_list) != 0 do
       Enum.each(counter_list, fn({counter, val}) ->
-        counter_name = get_key(counter, hostname)
+        counter_name = get_key(counter)
         ExStatsD.counter(val, counter_name)
       end)
     end
     :ok
   end
 
-  @spec get_key(String.t, String.t) :: String.t
-  defp get_key(val, hostname) do
-    "#{hostname}.loggerstatsd.#{val}"
-  end
-
-  @spec get_hostname :: String.t
-  defp get_hostname do
-    case :inet.gethostname do
-    {:ok, hostname} -> hostname
-    _ -> ""
-    end
+  @spec get_key(String.t) :: String.t
+  defp get_key(val) do
+    "loggerstatsd.#{val}"
   end
 end
